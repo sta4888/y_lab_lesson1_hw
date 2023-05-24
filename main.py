f234551234c80f68bb2e8f9682f11b73323e2554
eugene <talonin92@mail.ru>
2023-05-24 19:19:13 +0500

    0.0.0.3

--- main.py
+++ main.py
@@ -6,5 +6,18 @@ def domain_name(url: str) -> str:
     return url[:url.find('.')]
 
 
+def int32_to_ip(int32):
+    h = hex(int32)
+    rt = str(h)[2:]
+    if len(rt) < 8:
+        rt = rt.zfill(8)
+    ipV4 = []
+    for i in range(0, len(rt), 2):
+        s = rt[i] + rt[i+1]
+        ipV4.append(str(int(s, 16)))
+    return '.'.join(ipV4)
+
+
 if __name__ == "__main__":
-    print(domain_name("http://google.com"))
+    # print(domain_name("http://google.com"))
+    print(int32_to_ip(0))
--- test.py
+++ test.py
@@ -1,6 +1,12 @@
-from main import domain_name
+from main import domain_name, int32_to_ip
 
 assert domain_name("http://google.com") == "google"
 assert domain_name("http://google.co.jp") == "google"
 assert domain_name("www.xakep.ru") == "xakep"
 assert domain_name("https://youtube.com") == "youtube"
+
+
+
+assert int32_to_ip(2154959208) == "128.114.17.104"
+assert int32_to_ip(0) == "0.0.0.0"
+assert int32_to_ip(2149583361) == "128.32.10.1"
