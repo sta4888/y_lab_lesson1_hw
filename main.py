97ff12b8f6fc41fd460c95846a0e864d59ec9450
eugene <talonin92@mail.ru>
2023-05-24 22:05:52 +0500

    0.0.0.4

--- main.py
+++ main.py
@@ -13,11 +13,20 @@ def int32_to_ip(int32):
         rt = rt.zfill(8)
     ipV4 = []
     for i in range(0, len(rt), 2):
-        s = rt[i] + rt[i+1]
+        s = rt[i] + rt[i + 1]
         ipV4.append(str(int(s, 16)))
     return '.'.join(ipV4)
 
 
+def zeros(n):
+    result = 0
+    while n > 0:
+        n //= 5
+        result += n
+    return result
+
+
 if __name__ == "__main__":
     # print(domain_name("http://google.com"))
-    print(int32_to_ip(0))
+    # print(int32_to_ip(0))
+    print(zeros(30))
